273b37a10c03818e975a33a64a9760db84ce20c5
ivpcreative <ivpcreative@gmail.com>
2017-04-07 17:39:53 +0900

    wakai:add php

---
+++ themes/weluka-theme-00-child/page-sns.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The ssingle template file.
+ * pot or custom post
+ *Template Name:page-sns
+ * @package Weluka
+ * @since Weluka Theme 00 1.0
+ * 　
+ */
+get_header();
+global $weluka_themename;
+
+if ( have_posts() ) :
+    get_template_part( 'content', get_post_format() );
+    get_template_part( 'content-single-sns', 'none' );
+?>
+<?php
+else:
+	get_template_part( 'content', 'none' );
+endif;
+get_footer();
+?>
