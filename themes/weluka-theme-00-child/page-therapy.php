eef1633c285510c93f63ad741abcdeb6b8c2d4bc
ivpcreative <ivpcreative@gmail.com>
2017-04-12 18:21:49 +0900

    wakai:css ,page-therapy.php

--- themes/weluka-theme-00-child/page-therapy.php
+++ themes/weluka-theme-00-child/page-therapy.php
@@ -33,10 +33,10 @@ EOT;
 
     
 ?>
-<!--
+    <!--
 <div class="content-therapy">
 -->
-<?php    
+    <?php    
     
 echo $terapybtn ;
 
@@ -50,49 +50,52 @@ endif;
 echo $terapybtn
 ?>
 
-<div class="weluka-builder-content">
-<div id="" class="weluka-section " style="ls-therapy-webbtn">
-	<div class="weluka-container clearfix">    
-		<div id="" class="weluka-row clearfix">
-			<div id="" class="weluka-col weluka-col-md-12 ">
-				<div id="" class="weluka-button weluka-content weluka-text-center "><a href="#" class="weluka-btn weluka-btn-default">WEB予約へ</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-</div>
+        <div class="weluka-builder-content">
+            <div id="" class="weluka-section " style="ls-therapy-webbtn">
+                <div class="weluka-container clearfix">
+                    <div id="" class="weluka-row clearfix">
+                        <div id="" class="weluka-col weluka-col-md-12 ">
+                            <div id="" class="weluka-text-center p-therapy-contact">
+                                <a href="tel:0783252877" class="hidden-lg hidden-md hidden-sm"><img class="p-btn-therapy-contact" src="/wp-content/uploads/nav/ico-contact02.png" alt="お電話" /></a>
+                                <a href="/mail_reservation/" class=""><img class="p-btn-therapy-contact" src="/wp-content/uploads/nav/ico-contact01.png" alt="WEB予約" /></a>
 
-<!--
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--
 </div>-->
-<!--end class=therapy-->
+        <!--end class=therapy-->
 
-<script>
-jQuery(function () {
-    var pathname = location.pathname;
-    var therapyDetail = ""
-    var therapySum = ""
-    if(pathname.indexOf('-sum')!=-1){
-        /*URLに'-sum'が含まれている*/
-        jQuery(".js-therapy-sum").addClass("therapybtn-current");
-        therapyDetail = pathname.replace('-sum', '');
-        therapySum = pathname;
-    }else{
-        /*URLに'-sum'が含まれていない*/
-        jQuery(".js-therapy-detail").addClass("therapybtn-current");
-        therapyDetail = pathname;
-        var varSlash = pathname.split('/');
-        var splitMax = varSlash.length;
-        if(splitMax > 1 ){
-        therapySum = pathname.replace(varSlash[splitMax - 2] , varSlash[splitMax - 2] + '-sum');
-            } 
-    }
-    jQuery(".js-therapy-detail").attr("href", therapyDetail);
-    jQuery(".js-therapy-sum").attr("href", therapySum);
+        <script>
+            jQuery(function () {
+                var pathname = location.pathname;
+                var therapyDetail = ""
+                var therapySum = ""
+                if (pathname.indexOf('-sum') != -1) {
+                    /*URLに'-sum'が含まれている*/
+                    jQuery(".js-therapy-sum").addClass("therapybtn-current");
+                    therapyDetail = pathname.replace('-sum', '');
+                    therapySum = pathname;
+                } else {
+                    /*URLに'-sum'が含まれていない*/
+                    jQuery(".js-therapy-detail").addClass("therapybtn-current");
+                    therapyDetail = pathname;
+                    var varSlash = pathname.split('/');
+                    var splitMax = varSlash.length;
+                    if (splitMax > 1) {
+                        therapySum = pathname.replace(varSlash[splitMax - 2], varSlash[splitMax - 2] + '-sum');
+                    }
+                }
+                jQuery(".js-therapy-detail").attr("href", therapyDetail);
+                jQuery(".js-therapy-sum").attr("href", therapySum);
 
-});
-</script>
+            });
+        </script>
 
-<?php
+        <?php
 get_footer();
 ?>
